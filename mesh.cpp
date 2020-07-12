94c9938f06b8aa52406db00bd64a3c88b693f0a5
ankang1 <ankang1@v.systems>
2020-07-12 03:05:59 +0100

    update CAD

--- mesh.cpp
+++ mesh.cpp
@@ -1,8 +1,12 @@
 #include"MeshRegion.h"
 #include"Nektarpp.h"
 #include"LineEdge.h"
+#include"params.h"
+#include"Util.h"
 #include<iostream>
 using namespace std;
+double spanlength = 5.;
+double domainz = 7.;
 int N1 = 22;
 int N2 = 8;
 double p[3][2];
@@ -11,6 +15,20 @@ LineEdge line1(p[0], p[1], N1, QUDREFINE1, 0., 0.02);
 LineEdge line2(p[1], p[2], N2, EXPREFINE0, 0.02, 0.);
 
 double neawallRegion(double x, double y, double z) {
+    vector<double> p(3);
+    p[0] = x; p[1] = y; p[2] = z;
+    transform(p, -AoA);
+    double radius = rBoundaryLayer + Thickness*0.5;
+    double endz = spanlength + rBoundaryLayer;
+    double res = radius*radius - (x*x + y*y);
+    if(res>=0 && z <= endz && z>=-endz) return 1.;
+    else      return -1.;
+    double xend = chordLen + wakeLen;
+    if(p[0]>=0. && p[0]<=xend && p[1]>=-radius && p[1]<=radius && z <= endz && z>=-endz) return 1.;
+    else return -1;
+}
+
+double wakeRegion(double x, double y, double z) {
     return 1. - (x-0.5)*(x-0.5) - y*y;
 }
 
@@ -19,9 +37,9 @@ int main() {
     NektarppXml innerMesh("3DinUcomp.xml", "innermesh_", 1E-6);
     vector<double> targz1;
     vector<double> targz2;
-    p[0][0] = 0.; p[0][1] = 0.;
-    p[1][0] = 5.; p[1][1] = 0.;
-    p[2][0] = 7.; p[2][1] = 0.;
+    p[0][0] = 0.;         p[0][1] = 0.;
+    p[1][0] = spanlength; p[1][1] = 0.;
+    p[2][0] = domainz;    p[2][1] = 0.;
     vector<double> p;
     for(int i=0; i<N1; ++i) {
         double s = i*2./N1-1.;
