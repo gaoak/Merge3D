4feec6731c98bb36da917851befedb8eb79b266e
ankang1 <ankang1@v.systems>
2020-07-12 14:46:49 +0100

    update main

--- extractSurf.sh
+++ extractSurf.sh
@@ -1,6 +1,6 @@
-for n in  9 10
+for((n=0;n<9;++n))
 do
-NekMesh -m extract:surf=${n} test.xml surf${n}.xml
+NekMesh -m extract:surf=${n} testc.xml surf${n}.xml
 rm surf${n}.plt
 FieldConvert surf${n}.xml surf${n}.plt
 done
--- mesh.cpp
+++ mesh.cpp
@@ -9,12 +9,14 @@ double spanlength = 5.;
 double domainz = 7.;
 int N1 = 1;
 int N2 = 1;
-double p[3][2];
+static double anckerz[3][2];
+static void init() {
+    anckerz[0][0] = 0.;         anckerz[0][1] = 0.;
+    anckerz[1][0] = spanlength; anckerz[1][1] = 0.;
+    anckerz[2][0] = domainz;    anckerz[2][1] = 0.;
+}
 
-LineEdge line1(p[0], p[1], N1, QUDREFINE1, 0., 0.02);
-LineEdge line2(p[1], p[2], N2, EXPREFINE0, 0.02, 0.);
-
-double neawallRegion(double x, double y, double z) {
+static double neawallRegion(double x, double y, double z) {
     vector<double> p(3);
     p[0] = x; p[1] = y; p[2] = z;
     transform(p, -AoA);
@@ -22,24 +24,21 @@ double neawallRegion(double x, double y, double z) {
     double endz = spanlength + rBoundaryLayer;
     double res = radius*radius - (x*x + y*y);
     if(res>=0 && z <= endz && z>=-endz) return 1.;
-    else      return -1.;
     double xend = chordLen + wakeLen;
     if(p[0]>=0. && p[0]<=xend && p[1]>=-radius && p[1]<=radius && z <= endz && z>=-endz) return 1.;
     else return -1;
 }
 
-double wakeRegion(double x, double y, double z) {
-    return 1. - (x-0.5)*(x-0.5) - y*y;
+static double wakeRegion(double x, double y, double z) {
+    double radius = rBoundaryLayer + Thickness*0.5;
+    double endz = spanlength + rBoundaryLayer;
+    if(x>=0. && x<=farWakeRight && y>=-radius && y<=radius && z <= endz && z>=-endz) return 1.;
+    else return -1;
 }
 
-int main() {
-    NektarppXml baseMesh("3DoutUcomp.xml", "outmesh_", 1E-6);
-    NektarppXml innerMesh("3DinUcomp.xml", "innermesh_", 1E-6);
-    vector<double> targz1;
-    vector<double> targz2;
-    p[0][0] = 0.;         p[0][1] = 0.;
-    p[1][0] = spanlength; p[1][1] = 0.;
-    p[2][0] = domainz;    p[2][1] = 0.;
+static void setzscale(vector<double> &targz1, vector<double> &targz2) {
+    LineEdge line1(anckerz[0], anckerz[1], N1, QUDREFINE1, 0., hFirstLayer);
+    LineEdge line2(anckerz[1], anckerz[2], N2, EXPREFINE0, hFirstLayer, 0.);
     vector<double> p;
     for(int i=0; i<N1; ++i) {
         double s = i*2./N1-1.;
@@ -56,22 +55,30 @@ int main() {
     cout << endl;
     for(int i=0; i<targz2.size(); ++i) cout << targz2[i] << ", ";
     cout << endl;
+}
+
+int main() {
+    init();
+    NektarppXml baseMesh("3DoutUcomp.xml", "outmesh_", 1E-6);
+    NektarppXml innerMesh("3DinUcomp.xml", "innermesh_", 1E-6);
+    vector<double> targz1;
+    vector<double> targz2;
+    setzscale(targz1, targz2);
     baseMesh.LoadXml(N1+N2, targz1);
     innerMesh.LoadXml(N2, targz2);
-    vector<double> center={0., 0., 0.};
-    vector<vector<double> > centers;
-    centers.push_back(center);
-    vector<double> radius = {100.};
-    innerMesh.OutCompAsGeo("test.geo", centers, radius);
-    baseMesh.AddMeshRegion(innerMesh);
-    baseMesh.UpdateXml();
-    baseMesh.OutXml("test.xml");
     
+    baseMesh.AddMeshRegion(innerMesh);
     vector<void*> condition;
     condition.push_back((void *)neawallRegion);
     condition.push_back((void *)wakeRegion);
     baseMesh.ReorgDomain(condition);
-    baseMesh.UpdateXmlComposite();
-    baseMesh.UpdateXmlDomainExpansion();
-    baseMesh.OutXml("testc.xml");
+
+    baseMesh.UpdateXml();
+    baseMesh.OutXml("test.xml");
+
+    //vector<double> center={0., 0., 0.};
+    //vector<vector<double> > centers;
+    //centers.push_back(center);
+    //vector<double> radius = {100.};
+    //innerMesh.OutCompAsGeo("test.geo", centers, radius);
 }
