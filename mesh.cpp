e011519c2867c78fd19a7ef678142759bf12ac30
Ankang Gao <agao@blackfriars.ae.ic.ac.uk>
2020-07-13 11:53:55 +0100

    adjust

--- mesh.cpp
+++ mesh.cpp
@@ -7,13 +7,42 @@
 using namespace std;
 double spanlength = 5.;
 double domainz = 7.;
-int N1 = 25;
-int N2 = 5;
-static double anckerz[3][2];
+static double anckerz[4][2];
 static void init() {
     anckerz[0][0] = 0.;         anckerz[0][1] = 0.;
-    anckerz[1][0] = spanlength; anckerz[1][1] = 0.;
-    anckerz[2][0] = domainz;    anckerz[2][1] = 0.;
+    anckerz[1][0] = spanlength - 2.*chordLen; anckerz[1][1] = 0.;
+    anckerz[2][0] = spanlength; anckerz[2][1] = 0.;
+    anckerz[3][0] = domainz;    anckerz[3][1] = 0.;
+}
+
+static void setzscale(vector<double> &targz1, vector<double> &targz2) {
+    int N0 = 10;
+    int N1 = 15;
+    int N2 = 5;
+    LineEdge line0(anckerz[0], anckerz[1], N0, UNIFORM,    0., 0.);
+    LineEdge line1(anckerz[1], anckerz[2], N1, QUDREFINE1, 0., hFirstLayer);
+    LineEdge line2(anckerz[2], anckerz[3], N2, EXPREFINE0, hFirstLayer, 0.);
+    vector<double> p;
+    for(int i=0; i<line0.m_N; ++i) {
+        double s = i*2./line0.m_N-1.;
+        p = line0.Evaluate(s);
+        targz1.push_back(p[0]);
+    }
+    for(int i=0; i<line1.m_N; ++i) {
+        double s = i*2./line1.m_N-1.;
+        p = line1.Evaluate(s);
+        targz1.push_back(p[0]);
+    }
+    for(int i=0; i<=line2.m_N; ++i) {
+        double s = i*2./line2.m_N-1.;
+        p = line2.Evaluate(s);
+        targz1.push_back(p[0]);
+        targz2.push_back(p[0]);
+    }
+    for(int i=0; i<targz1.size(); ++i) cout << targz1[i] << ", ";
+    cout << endl;
+    for(int i=0; i<targz2.size(); ++i) cout << targz2[i] << ", ";
+    cout << endl;
 }
 
 static double neawallRegion(double x, double y, double z) {
@@ -38,36 +67,15 @@ static double wakeRegion(double x, double y, double z) {
     else return -1;
 }
 
-static void setzscale(vector<double> &targz1, vector<double> &targz2) {
-    LineEdge line1(anckerz[0], anckerz[1], N1, QUDREFINE1, 0., hFirstLayer);
-    LineEdge line2(anckerz[1], anckerz[2], N2, EXPREFINE0, hFirstLayer, 0.);
-    vector<double> p;
-    for(int i=0; i<N1; ++i) {
-        double s = i*2./N1-1.;
-        p = line1.Evaluate(s);
-        targz1.push_back(p[0]);
-    }
-    for(int i=0; i<=N2; ++i) {
-        double s = i*2./N2-1.;
-        p = line2.Evaluate(s);
-        targz1.push_back(p[0]);
-        targz2.push_back(p[0]);
-    }
-    for(int i=0; i<targz1.size(); ++i) cout << targz1[i] << ", ";
-    cout << endl;
-    for(int i=0; i<targz2.size(); ++i) cout << targz2[i] << ", ";
-    cout << endl;
-}
-
 int main() {
     init();
-    NektarppXml baseMesh("3DoutUcomp.xml", "outmesh_", 1E-6);
-    NektarppXml innerMesh("3DinUcomp.xml", "innermesh_", 1E-6);
+    NektarppXml baseMesh("../Mesh2D/3DoutUcomp.xml", "outmesh_", 1E-6);
+    NektarppXml innerMesh("../Mesh2D/3DinUcomp.xml", "innermesh_", 1E-6);
     vector<double> targz1;
     vector<double> targz2;
     setzscale(targz1, targz2);
-    baseMesh.LoadXml(N1+N2, targz1);
-    innerMesh.LoadXml(N2, targz2);
+    baseMesh.LoadXml(targz1.size()-1, targz1);
+    innerMesh.LoadXml(targz2.size()-1, targz2);
     
     baseMesh.AddMeshRegion(innerMesh);
     vector<void*> condition;
