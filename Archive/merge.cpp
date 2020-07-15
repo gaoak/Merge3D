136127d1af5a755aeaa02e2a5000079739e9489c
ankang1 <ankang1@v.systems>
2020-07-15 20:19:23 +0100

    to su2

--- Archive/merge.cpp
+++ Archive/merge.cpp
@@ -1,3 +1,6 @@
+/*
+merge 3D nektar mesh at Re 400
+*/
 #include"MeshRegion.h"
 #include"Nektarpp.h"
 #include"LineEdge.h"
@@ -16,9 +19,9 @@ static void init() {
 }
 
 static void setzscale(vector<double> &targz1, vector<double> &targz2) {
-    int N0 = 10;
-    int N1 = 60;
-    int N2 = 20;
+    int N0 = 5;
+    int N1 = 20;
+    int N2 = 7;
     LineEdge line0(anckerz[0], anckerz[1], N0, QUDREFINE1,    0., 0.1);
     LineEdge line1(anckerz[1], anckerz[2], N1, QUDREFINE1, 0., hFirstLayer);
     LineEdge line2(anckerz[2], anckerz[3], N2, EXPREFINE0, hFirstLayer, 0.);
@@ -93,12 +96,20 @@ int main() {
     condition.push_back((void *)neawallRegion);
     condition.push_back((void *)wakeRegion);
 
+    NektarppXml mesh2D("../Mesh2D/outerRegion.xml", "2Doutmesh_", 1E-6);
+    mesh2D.LoadXml(targz1.size()-1, targz1);
+    mesh2D.ReorgDomain(condition);
+    mesh2D.UpdateXml();
+    mesh2D.OutXml("test2D.xml");
+
     NektarppXml baseMesh("../Mesh2D/3DoutUcomp.xml", "outmesh_", 1E-6);
     NektarppXml innerMesh("../Mesh2D/3DinUcomp.xml", "innermesh_", 1E-6);
     baseMesh.LoadXml(targz1.size()-1, targz1);
     innerMesh.LoadXml(targz2.size()-1, targz2);
     baseMesh.AddMeshRegion(innerMesh);
-    baseMesh.OutPutSU2("test.su2");
+    baseMesh.ReorgDomain(condition);
+    baseMesh.UpdateXml();
+    baseMesh.OutXml("test.xml");
 
     //vector<double> center={0., 0., 0.};
     //vector<vector<double> > centers;
