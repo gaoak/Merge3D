6fa2dbf40b0f0e06bfa549c314a041eaccb47bbe
ankang1 <ankang1@v.systems>
2020-10-09 21:50:43 +0100

    three regions

--- mesh.cpp
+++ mesh.cpp
@@ -12,46 +12,40 @@ using namespace std;
 double spanlength = 5.;
 double domainz = 7.;
 static double anckerz[4][2];
+double offset = hFirstLayer;
 static void init() {
     anckerz[0][0] = 0.;         anckerz[0][1] = 0.;
-    anckerz[1][0] = spanlength - 0.2; anckerz[1][1] = 0.;
-    anckerz[2][0] = spanlength; anckerz[2][1] = 0.;
-    anckerz[3][0] = domainz - 1.2*chordLen;    anckerz[3][1] = 0.;
+    anckerz[1][0] = spanlength; anckerz[1][1] = 0.;
+    anckerz[2][0] = spanlength + offset; anckerz[2][1] = 0.;
+    anckerz[3][0] = domainz;    anckerz[3][1] = 0.;
 }
 
-static void setzscale(vector<double> &targz1, vector<double> &targz2) {
-    int N0 = 35;
-    int N1 = 5;
-    int N2 = 10;
-    LineEdge line0(anckerz[0], anckerz[1], N0, UNIFORM,  0., 10.*hFirstLayer);
-    LineEdge line1(anckerz[1], anckerz[2], N1, EXPREFINE1, 0., hFirstLayer);
-    LineEdge line2(anckerz[2], anckerz[3], N2-1, EXPREFINE0, progress*hFirstLayer, 0.);
+static void setzscale(vector<double> &targz1, vector<double> &targz2, vector<double> &targz3) {
+    int N0 = 40;
+    int N1 = 9;
+    targz1.clear();
+    targz2.clear();
+    targz3.clear();
+    LineEdge line0(anckerz[0], anckerz[1], N0, BOUNDARYLAYER1, 0, 0, 0, 0.5*offset, 3., 5);
+    LineEdge line1(anckerz[2], anckerz[3], N1, BOUNDARYLAYER0, 1.5*offset, 2., 5, 0, 0, 0);
     vector<double> p;
-    for(int i=0; i<line0.m_N; ++i) {
+    for(int i=0; i<=line0.m_N; ++i) {
         double s = i*2./line0.m_N-1.;
         p = line0.Evaluate(s);
         targz1.push_back(p[0]);
     }
-    for(int i=0; i<line1.m_N; ++i) {
+    targz3.push_back(anckerz[1][0]);
+    for(int i=0; i<=line1.m_N; ++i) {
         double s = i*2./line1.m_N-1.;
         p = line1.Evaluate(s);
-        targz1.push_back(p[0]);
-    }
-    targz1[targz1.size()-1] = spanlength - hFirstLayer;
-    for(int i=0; i<=line2.m_N; ++i) {
-        double s = i*2./line2.m_N-1.;
-        p = line2.Evaluate(s);
-        targz1.push_back(p[0]);
         targz2.push_back(p[0]);
+        targz3.push_back(p[0]);
     }
-    targz1.push_back(domainz);
-    targz2.push_back(domainz);
-    //if(targz1[N0+N1] - targz1[N0+N1-1]>hFirstLayer) targz1[N0+N1-1] = targz1[N0+N1] - hFirstLayer;
-    if(targz1[N0+N1+1] - targz1[N0+N1]>hFirstLayer) targz1[N0+N1+1] = targz1[N0+N1] + hFirstLayer;
-    if(targz2[1]       -     targz2[0]>hFirstLayer) targz2[1]       = targz2[0]     + hFirstLayer;
     for(int i=0; i<targz1.size(); ++i) cout << targz1[i] << ", ";
     cout << endl;
     for(int i=0; i<targz2.size(); ++i) cout << targz2[i] << ", ";
+    cout << endl;
+    for(int i=0; i<targz3.size(); ++i) cout << targz3[i] << ", ";
     cout << endl;
     cout << "input c for continue " << endl;
     char c = 0;
@@ -72,9 +66,8 @@ static double detectSingular(double x, double y, double z) {
 
 int main() {
     init();
-    vector<double> targz1;
-    vector<double> targz2;
-    setzscale(targz1, targz2);
+    vector<double> targz1, targz2, targz3;
+    setzscale(targz1, targz2, targz3);
     vector<void*> condition;
     condition.push_back((void *)detectSingular);
     condition.push_back((void *)neawallRegion);
@@ -83,12 +76,24 @@ int main() {
     mesh2D.LoadXml(targz2.size()-1, targz2);
     mesh2D.ReorgDomain(condition, false);
     mesh2D.UpdateXml();
-    mesh2D.OutXml("test2D.xml");
+    //mesh2D.OutXml("test2D.xml");
 
     NektarppXml baseMesh("../Mesh2D/3DoutUcomp.xml", "outmesh_", 1E-6);
-    NektarppXml innerMesh("../Mesh2D/3DinUcomp.xml", "innermesh_", 1E-6);
-    baseMesh.LoadXml(targz1.size()-1, targz1);
-    innerMesh.LoadXml(targz2.size()-1, targz2);
+    baseMesh.LoadWallmapping("wallmapping.dat");
+    baseMesh.LoadXml(targz1.size()-1, targz1, 0., offset);
+    //baseMesh.OutXml("testouter1.xml");
+
+    NektarppXml baseMesh2("../Mesh2D/3DoutUcomp2.xml", "outmesh_", 1E-6);
+    baseMesh2.LoadWallmapping("wallmapping.dat");
+    baseMesh2.LoadXml(targz2.size()-1, targz2, 0., 0.);
+    baseMesh2.OutXml("testouter2.xml");
+
+    NektarppXml innerMesh("../Mesh2D/3DinUcomp.xml", "outmesh_", 1E-6);
+    innerMesh.LoadWallmapping("wallmapping.dat");
+    innerMesh.LoadXml(targz3.size()-1, targz3, 0., 0., true, 0);
+    //innerMesh.OutXml("testinner.xml");
+
+    baseMesh.AddMeshRegion(baseMesh2);
     baseMesh.AddMeshRegion(innerMesh);
     baseMesh.ReorgDomain(condition, true);
     baseMesh.UpdateXml();
