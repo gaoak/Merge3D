a6a8d5dea7e18849e304ca5c1b13859a79c10ed1
ankang1 <ankang1@v.systems>
2020-10-10 15:03:10 +0100

    check mesh

--- mesh.cpp
+++ mesh.cpp
@@ -27,7 +27,7 @@ static void setzscale(vector<double> &targz1, vector<double> &targz2, vector<dou
     targz2.clear();
     targz3.clear();
     LineEdge line0(anckerz[0], anckerz[1], N0, BOUNDARYLAYER1, 0, 0, 0, offset, 2., 5);
-    LineEdge line1(anckerz[2], anckerz[3], N1, BOUNDARYLAYER0, 1.5*offset, 2., 5, 0, 0, 0);
+    LineEdge line1(anckerz[2], anckerz[3], N1, BOUNDARYLAYER0, 2.*offset, 2., 7, 0, 0, 0);
     vector<double> p;
     for(int i=0; i<=line0.m_N; ++i) {
         double s = i*2./line0.m_N-1.;
@@ -96,8 +96,10 @@ int main() {
 
     baseMesh.AddMeshRegion(baseMesh2);
     baseMesh.AddMeshRegion(innerMesh);
+    baseMesh.ReorgBoundary();
     baseMesh.ReorgDomain(condition, true);
     baseMesh.UpdateXml();
+    baseMesh.CheckMesh();
     baseMesh.OutXml("test.xml");
 
     return 0;
