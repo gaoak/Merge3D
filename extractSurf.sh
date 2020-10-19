20d0354e39938efb6459e6137b80eb5059cc51a3
ankang1 <ankang1@v.systems>
2020-10-19 18:37:24 +0100

    fullwing

--- extractSurf.sh
+++ extractSurf.sh
@@ -1,4 +1,4 @@
-for((n=0;n<8;++n))
+for((n=0;n<=8;++n))
 do
 NekMesh -m extract:surf=${n} testcomp.xml surf${n}.xml
 rm surf${n}.plt
--- mesh.cpp
+++ mesh.cpp
@@ -12,12 +12,17 @@ using namespace std;
 double spanlength = 5.;
 double domainz = 7.;
 static double anckerz[4][2];
+static double anckerz_neg[4][2];
 double offset = hFirstLayer;
 static void init() {
-    anckerz[0][0] = 0.;         anckerz[0][1] = 0.;
-    anckerz[1][0] = spanlength; anckerz[1][1] = 0.;
+    anckerz_neg[0][0] = 0.;                   anckerz_neg[0][1] = 0.;
+    anckerz_neg[1][0] = -spanlength;          anckerz_neg[1][1] = 0.;
+    anckerz_neg[2][0] = -spanlength - offset; anckerz_neg[2][1] = 0.;
+    anckerz_neg[3][0] = -domainz;             anckerz_neg[3][1] = 0.;
+    anckerz[0][0] = 0.;                  anckerz[0][1] = 0.;
+    anckerz[1][0] = spanlength;          anckerz[1][1] = 0.;
     anckerz[2][0] = spanlength + offset; anckerz[2][1] = 0.;
-    anckerz[3][0] = domainz;    anckerz[3][1] = 0.;
+    anckerz[3][0] = domainz;             anckerz[3][1] = 0.;
 }
 
 static void setzscale(vector<double> &targz1, vector<double> &targz2, vector<double> &targz3) {
@@ -68,7 +73,17 @@ static double detectSingular(double x, double y, double z) {
 int main() {
     init();
     vector<double> targz1, targz2, targz3;
+    vector<double> targz1_n, targz2_n, targz3_n;
     setzscale(targz1, targz2, targz3);
+    for(int i=targz1.size()-1; i>=0; --i) {
+        targz1_n.push_back(-targz1[i]);
+    }
+    for(int i=targz2.size()-1; i>=0; --i) {
+        targz2_n.push_back(-targz2[i]);
+    }
+    for(int i=targz3.size()-1; i>=0; --i) {
+        targz3_n.push_back(-targz3[i]);
+    }
     vector<void*> condition;
     condition.push_back((void *)detectSingular);
     condition.push_back((void *)neawallRegion);
@@ -82,20 +97,33 @@ int main() {
     NektarppXml baseMesh("../Mesh2D/3DoutUcomp.xml", "outmesh_", 1E-6);
     baseMesh.LoadWallmapping("wallmapping.dat");
     baseMesh.LoadXml(targz1.size()-1, targz1, 0., offset);
+    NektarppXml baseMesh_negative("../Mesh2D/3DoutUcomp.xml", "outmesh_neg", 1E-6);
+    baseMesh_negative.LoadWallmapping("wallmapping.dat");
+    baseMesh_negative.LoadXml(targz1_n.size()-1, targz1_n, offset, 0.);
     //baseMesh.OutXml("testouter1.xml");
 
     NektarppXml baseMesh2("../Mesh2D/3DoutUcomp2.xml", "outmesh_", 1E-6);
     baseMesh2.LoadWallmapping("wallmapping.dat");
     baseMesh2.LoadXml(targz2.size()-1, targz2, 0., 0.);
     baseMesh2.OutXml("testouter2.xml");
+    NektarppXml baseMesh2_negative("../Mesh2D/3DoutUcomp2.xml", "outmesh_", 1E-6);
+    baseMesh2_negative.LoadWallmapping("wallmapping.dat");
+    baseMesh2_negative.LoadXml(targz2_n.size()-1, targz2_n, 0., 0.);
+    baseMesh2_negative.OutXml("testouter2.xml");
 
     NektarppXml innerMesh("../Mesh2D/3DinUcomp.xml", "outmesh_", 1E-6);
     innerMesh.LoadWallmapping("wallmapping.dat");
     innerMesh.LoadXml(targz3.size()-1, targz3, 0., 0., true, 0);
+    NektarppXml innerMesh_negative("../Mesh2D/3DinUcomp.xml", "outmesh_", 1E-6);
+    innerMesh_negative.LoadWallmapping("wallmapping.dat");
+    innerMesh_negative.LoadXml(targz3_n.size()-1, targz3_n, 0., 0., true, targz3_n.size()-1);
     //innerMesh.OutXml("testinner.xml");
 
+    baseMesh.AddMeshRegion(baseMesh_negative);
     baseMesh.AddMeshRegion(baseMesh2);
+    baseMesh.AddMeshRegion(baseMesh2_negative);
     baseMesh.AddMeshRegion(innerMesh);
+    baseMesh.AddMeshRegion(innerMesh_negative);
     baseMesh.ReorgBoundary();
     baseMesh.ReorgDomain(condition, true);
     baseMesh.UpdateXml();
