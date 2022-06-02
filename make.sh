9f29f71ba585d1874d2d3208b6039a08c0e49438
ankang <ankanggao@ustc.edu.cn>
2022-06-02 19:54:23 +0800

    update mesh.cpp

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-c++ -DSWEPTANGLE=15 -std=c++11 -I./CAD3D/ -I../Mesh2D/ *.cpp CAD3D/*.cpp -c -Og -g
+c++ -std=c++11 -I./CAD3D/ -I../Mesh2D/ *.cpp CAD3D/*.cpp -c -Og -g
 c++ -std=c++11 -o mesh-g *.o  -g
 ./mesh-g
 if [ "0" = "$?" ]; then
