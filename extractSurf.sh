4aa6d7ff165e1154d923cdd5d6433fbf596560ea
Ankang Gao <ankanggao@ustc.edu.cn>
2024-05-13 13:55:11 +0800

    update extractsurf.sh

--- extractSurf.sh
+++ extractSurf.sh
@@ -1,6 +1,6 @@
 for((n=0;n<8;++n))
 do
-NekMesh -m extract:surf=${n} testcomp.xml surf${n}.xml
+NekMesh -m extract:surf=${n} testcomp.xml surf${n}.xml -f
 rm surf${n}.plt
-FieldConvert surf${n}.xml surf${n}.plt
+FieldConvert surf${n}.xml surf${n}.plt -f
 done
