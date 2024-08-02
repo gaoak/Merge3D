92ad40b29b36a0bae32d1381b85a382f8de9f58d
Ankang Gao <ankanggao@ustc.edu.cn>
2024-08-02 22:05:52 +0800

    3D grid for FIV

--- Archive/FIV800/Span1.h
+++ Archive/FIV800/Span1.h
@@ -1,5 +1,5 @@
 double hFirstLayer = 0.01;
-int wingn = 12 ; // value used by bash shell should be constant number
+int wingn = 21 ; // value used by bash shell should be constant number
 int tipn = 13 ; // value used by bash shell should be constant number
-double spanlength = 0.5;
+double spanlength = 1.;
 double domainz = spanlength + 3.;
