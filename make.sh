02681176f532e3dd003668f7765bec54bde6d10d
Ankang Gao <ankanggao@ustc.edu.cn>
2025-05-20 16:57:10 +0800

    fix

--- make.sh
+++ make.sh
@@ -3,9 +3,11 @@ if [ ! -f 3DoutUcomp.xml ]; then
   bash extrude.sh
 fi
 cd ..
-c++ -std=c++11 *.cpp CAD3D/*.cpp -c -Og -g
-c++ -std=c++11 -o mesh-g *.o  -g
-./mesh-g
+cd build
+cmake ..
+make
+cd ..
+./build/mesh
 if [ "0" = "$?" ]; then
     rm testcomp.xml
     NekMesh test.xml testcomp.xml
--- params3D.h
+++ params3D.h
@@ -1,4 +1,4 @@
-double hFirstLayer = 0.01;
+double hFirstLayer = 0.05;
 int wingn = 25 ; // value used by bash shell should be constant number
 int tipn = 13 ; // value used by bash shell should be constant number
 double tipoffset = 0.1;
